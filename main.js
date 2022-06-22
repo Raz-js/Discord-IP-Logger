3fcf16a4127551ac31bc51fa8dec087c6860114b
Raz <91196395+Raz-js@users.noreply.github.com>
2022-06-22 20:27:06 +0200

    Beautified 🌸

--- main.js
+++ main.js
@@ -10,139 +10,139 @@ if (window.TKInjected && !window.TKInjectedDebug) {
 window.TKInjected = true
 
 //Const-Echo
-		var echo = (function() {
+var echo = (function() {
 
-			var queue = [];
-			var ECHO_TOKEN = {};
-			var RESET_INPUT = "%c ";
-			var RESET_CSS = "";
+    var queue = [];
+    var ECHO_TOKEN = {};
+    var RESET_INPUT = "%c ";
+    var RESET_CSS = "";
 
-			// Attach formatting utility method.
-			function successFormatting( value ) {
+    // Attach formatting utility method.
+    function successFormatting(value) {
 
-				queue.push({
-					value: value,
-					css: "display: inline-block ; background-color: #1DB954 ; color: #191414 ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ;"
-				});
+        queue.push({
+            value: value,
+            css: "display: inline-block ; background-color: #1DB954 ; color: #191414 ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ;"
+        });
 
-				return( ECHO_TOKEN );
+        return (ECHO_TOKEN);
 
-			}
+    }
 
-			// Attach formatting utility method.
-			function warningFormatting( value ) {
+    // Attach formatting utility method.
+    function warningFormatting(value) {
 
-				queue.push({
-					value: value,
-					css: "display: inline-block ; background-color: gold ; color: black ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ;"
-				});
+        queue.push({
+            value: value,
+            css: "display: inline-block ; background-color: gold ; color: black ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ;"
+        });
 
-				return( ECHO_TOKEN );
+        return (ECHO_TOKEN);
 
-			}
+    }
 
-			// I provide an echo-based proxy to the given Console Function. This uses an
-			// internal queue to aggregate values before calling the given Console
-			// Function with the desired formatting.
-			function using( consoleFunction ) {
+    // I provide an echo-based proxy to the given Console Function. This uses an
+    // internal queue to aggregate values before calling the given Console
+    // Function with the desired formatting.
+    function using(consoleFunction) {
 
-				function consoleFunctionProxy() {
+        function consoleFunctionProxy() {
 
-					// As we loop over the arguments, we're going to aggregate a set of
-					// inputs and modifiers. The Inputs will ultimately be collapsed down
-					// into a single string that acts as the first console.log parameter
-					// while the modifiers are then SPREAD into console.log as 2...N.
-					// --
-					// NOTE: After each input/modifier pair, I'm adding a RESET pairing.
-					// This implicitly resets the CSS after every formatted pairing.
-					var inputs = [];
-					var modifiers = [];
+            // As we loop over the arguments, we're going to aggregate a set of
+            // inputs and modifiers. The Inputs will ultimately be collapsed down
+            // into a single string that acts as the first console.log parameter
+            // while the modifiers are then SPREAD into console.log as 2...N.
+            // --
+            // NOTE: After each input/modifier pair, I'm adding a RESET pairing.
+            // This implicitly resets the CSS after every formatted pairing.
+            var inputs = [];
+            var modifiers = [];
 
-					for ( var i = 0 ; i < arguments.length ; i++ ) {
+            for (var i = 0; i < arguments.length; i++) {
 
-						// When the formatting utility methods are called, they return
-						// a special token. This indicates that we should pull the
-						// corresponding value out of the QUEUE instead of trying to
-						// output the given argument directly.
-						if ( arguments[ i ] === ECHO_TOKEN ) {
+                // When the formatting utility methods are called, they return
+                // a special token. This indicates that we should pull the
+                // corresponding value out of the QUEUE instead of trying to
+                // output the given argument directly.
+                if (arguments[i] === ECHO_TOKEN) {
 
-							var item = queue.shift();
+                    var item = queue.shift();
 
-							inputs.push( ( "%c" + item.value ), RESET_INPUT );
-							modifiers.push( item.css, RESET_CSS );
+                    inputs.push(("%c" + item.value), RESET_INPUT);
+                    modifiers.push(item.css, RESET_CSS);
 
-						// For every other argument type, output the value directly.
-						} else {
+                    // For every other argument type, output the value directly.
+                } else {
 
-							var arg = arguments[ i ];
+                    var arg = arguments[i];
 
-							if (
-								( typeof( arg ) === "object" ) ||
-								( typeof( arg ) === "function" )
-								) {
+                    if (
+                        (typeof(arg) === "object") ||
+                        (typeof(arg) === "function")
+                    ) {
 
-								inputs.push( "%o", RESET_INPUT );
-								modifiers.push( arg, RESET_CSS );
+                        inputs.push("%o", RESET_INPUT);
+                        modifiers.push(arg, RESET_CSS);
 
-							} else {
+                    } else {
 
-								inputs.push( ( "%c" + arg ), RESET_INPUT );
-								modifiers.push( RESET_CSS, RESET_CSS );
+                        inputs.push(("%c" + arg), RESET_INPUT);
+                        modifiers.push(RESET_CSS, RESET_CSS);
 
-							}
+                    }
 
-						}
+                }
 
-					}
+            }
 
-					consoleFunction( inputs.join( "" ), ...modifiers );
+            consoleFunction(inputs.join(""), ...modifiers);
 
-					// Once we output the aggregated value, reset the queue. This should have
-					// already been emptied by the .shift() calls; but the explicit reset
-					// here acts as both a marker of intention as well as a fail-safe.
-					queue = [];
+            // Once we output the aggregated value, reset the queue. This should have
+            // already been emptied by the .shift() calls; but the explicit reset
+            // here acts as both a marker of intention as well as a fail-safe.
+            queue = [];
 
-				}
+        }
 
-				return( consoleFunctionProxy );
+        return (consoleFunctionProxy);
 
-			}
+    }
 
-			return({
-				// Console(ish) functions.
-				log: using( console.log ),
-				warn: using( console.warn ),
-				error: using( console.error ),
-				trace: using( console.trace ),
-				group: using( console.group ),
-				groupEnd: using( console.groupEnd ),
+    return ({
+        // Console(ish) functions.
+        log: using(console.log),
+        warn: using(console.warn),
+        error: using(console.error),
+        trace: using(console.trace),
+        group: using(console.group),
+        groupEnd: using(console.groupEnd),
 
-				// Formatting functions.
-				asSuccess: successFormatting,
-				asWarning: warningFormatting
-			});
+        // Formatting functions.
+        asSuccess: successFormatting,
+        asWarning: warningFormatting
+    });
 
-		})();
+})();
 
 //Delete-Class-Func
-function removeElementsByClass(className){
+function removeElementsByClass(className) {
     const elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
+    while (elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
     }
 }
 
 //Theme-Const
 const injectTKCss = (id, css) => {
-  const style = document.createElement("style")
-  style.setAttribute(`css`, id)
-  style.innerHTML = css
-  document.head.appendChild(style)
+    const style = document.createElement("style")
+    style.setAttribute(`css`, id)
+    style.innerHTML = css
+    document.head.appendChild(style)
 }
 
 const ejectTKCss = (id) => {
-  const style = document.querySelector(`style[css="${id}"]`)
-  if (style) style.remove()
+    const style = document.querySelector(`style[css="${id}"]`)
+    if (style) style.remove()
 }
 //End
 
@@ -257,26 +257,32 @@ const toastWrapper = document.createElement("div");
 toastWrapper.className = "dt-toast-wrapper";
 toastWrapper.style.marginBottom = "5px";
 toastContainer.appendChild(toastWrapper);
+
 function Timer(callback, delay) {
     let timerId, start, remaining = delay;
-    const pause = function () {
+    const pause = function() {
         window.clearTimeout(timerId);
         timerId = null;
         remaining -= Date.now() - start;
     };
-    const resume = function () {
+    const resume = function() {
         if (timerId)
             return;
         start = Date.now();
         timerId = window.setTimeout(callback, remaining);
     };
     resume();
-    return { pause, resume };
+    return {
+        pause,
+        resume
+    };
 }
 
 //Toast-Const
-function ls8sdajw8asjdia(text, options={}) {
-    const { type = "success", duration = 3000, autoClose = true, closeButton = true } = options;
+function ls8sdajw8asjdia(text, options = {}) {
+    const {
+        type = "success", duration = 3000, autoClose = true, closeButton = true
+    } = options;
     if (!autoClose && !closeButton)
         throw new Error("You can't have autoClose and closeButton disabled");
     const toast = document.createElement("div");
@@ -293,6 +299,7 @@ function ls8sdajw8asjdia(text, options={}) {
     toastMessage.className = "dt-toast-message";
     toastMessage.innerText = text;
     toastMessageWrapper.appendChild(toastMessage);
+
     function removeToast() {
         toast.classList.add("removing");
         setTimeout(() => toast.remove(), 300);
@@ -313,8 +320,8 @@ function ls8sdajw8asjdia(text, options={}) {
 //----------------------------------------------
 
 //Theme-Func
-function theme () {
-injectTKCss("Original", `.logo { color: #f00 }
+function theme() {
+    injectTKCss("Original", `.logo { color: #f00 }
 .epWhU7hHGktzlO_dop6z { background-color: #b71010 }
 .TywOcKZEqNynWecCiATc { --is-active-fg-color: #46170a }
 .hDgDGI { color: red }
@@ -330,14 +337,14 @@ injectTKCss("Original", `.logo { color: #f00 }
 
 //Help-Command
 var tk = {
-  get help () {
-    echo.log(echo.asWarning( "If nothing popped up in the corner of your screen, please go to our discord server | discord.gg/TZYzNPBN7C |" ));
-    ls8sdajw8asjdia("Edit your theme via the non-obfusticated part\nof the code and use the toggle theme\nbutton on spotify to execute your theme <3")
-  },
-  get info () {
-    echo.log(echo.asWarning( "If nothing popped up in the corner of your screen, please go to our discord server | discord.gg/TZYzNPBN7C |" ));
-    ls8sdajw8asjdia("𝐂𝐡𝐚𝐧𝐠𝐞𝐥𝐨𝐠:\n- Premium Hack\n- Theme Changer\nᴏᴛʜᴇʀ: Help = tk.help")
-  }
+    get help() {
+        echo.log(echo.asWarning("If nothing popped up in the corner of your screen, please go to our discord server | discord.gg/TZYzNPBN7C |"));
+        ls8sdajw8asjdia("Edit your theme via the non-obfusticated part\nof the code and use the toggle theme\nbutton on spotify to execute your theme <3")
+    },
+    get info() {
+        echo.log(echo.asWarning("If nothing popped up in the corner of your screen, please go to our discord server | discord.gg/TZYzNPBN7C |"));
+        ls8sdajw8asjdia("𝐂𝐡𝐚𝐧𝐠𝐞𝐥𝐨𝐠:\n- Premium Hack\n- Theme Changer\nᴏᴛʜᴇʀ: Help = tk.help")
+    }
 }
 
 //Main-Button
@@ -347,23 +354,20 @@ tkBtn.className = "tkBtn";
 tkBtn.id = "tkBtn";
 tkBtn.style = "background-color: #181818; padding: 0; border: none";
 tkBtn.value = "OFF";
-tkBtn.onclick = function () {
-  ls8sdajw8asjdia("Toggled Theme")
- if(document.getElementById("tkBtn").value=="OFF")
-              {
-               document.getElementById("tkBtn").value="ON";
-	       theme()
-              }
-              else
-              {
-                document.getElementById("tkBtn").value="OFF";
-		ejectTKCss("Original")
-              }
+tkBtn.onclick = function() {
+    ls8sdajw8asjdia("Toggled Theme")
+    if (document.getElementById("tkBtn").value == "OFF") {
+        document.getElementById("tkBtn").value = "ON";
+        theme()
+    } else {
+        document.getElementById("tkBtn").value = "OFF";
+        ejectTKCss("Original")
+    }
 };
 document.body.appendChild(tkBtn);
 
 //Mute-Spotify-Ads
-!async function () {
+!async function() {
 
     async function queryAsync(query) {
         return new Promise(resolve => {
@@ -409,9 +413,9 @@ ls8sdajw8asjdia("Injected Spotify Toolkit By Raz <3")
 
 //After-Inject
 let title_el = document.querySelector("title");
-if(title_el)
+if (title_el)
     title_el.innerHTML = "[Injected] Spotify Toolkit";
-if(clear) clear()
+if (clear) clear()
 else console.clear()
-echo.log(echo.asSuccess( "[Injected] Spotify Toolkit" ))
+echo.log(echo.asSuccess("[Injected] Spotify Toolkit"))
 console.log('%cFor more info use the command: "tk.info" below 👇', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; font-style: italic; border: 2px solid white; margin: 30px;');
